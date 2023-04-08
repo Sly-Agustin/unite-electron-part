0d436646eb63b9f018a4a5e2fa80be2bc398c910
sly-agustin <agustin_sly@hotmail.com>
2023-04-08 14:49:00 -0300

    feat: first iteration of allGames render

---
+++ src/components/AllGames.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+const AllGames = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  // The empty array at the end is so we renderize this just the first time
+  useEffect(() => {
+    const getGames = async() => {
+      try{
+        const response = await fetch('http://localhost:5001/games/all');
+        const games = await response.json(); 
+        setData(games);
+        setIsLoading(false);
+      }
+      catch(err){
+        console.log('Something bad happened, try again later')
+      }
+    }
+
+    getGames();
+  }, []);
+
+  if(isLoading){
+    return(
+      <Fragment>
+        <h1>All games dummy</h1>
+        <div className="App">
+          <h2>Cargando...</h2>
+        </div>
+      </Fragment>
+    )
+  }
+
+  return(
+    <Fragment>
+      <h1>All games dummy</h1>
+      <div className="App">
+        <h2>Juegos</h2>
+        <ul> 
+          {
+            data.games.map((item, index) =>
+              <li key={index}>{item.name}</li>
+            )
+          }
+        </ul>
+      </div>
+    </Fragment>
+  )
+  
+}
+
+export default AllGames;
