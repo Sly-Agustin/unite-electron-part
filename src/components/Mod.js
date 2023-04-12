77a843a77583e6a6dee6f512e436a28126a4805d
sly-agustin <agustin_sly@hotmail.com>
2023-04-12 02:03:15 -0300

    feat: download of mod files

--- src/components/Mod.js
+++ src/components/Mod.js
@@ -24,6 +24,36 @@ const Mod = () => {
     getMod();
   }, [])
 
+  const downloadFile = () => {
+    const download = async () => {
+      const response = await fetch(`${process.env.REACT_APP_HOST}/mod/${id}/file`)
+      console.log(response)
+      const responseBlob = await response.blob();
+      
+      const url = window.URL.createObjectURL(
+        new Blob([responseBlob]),
+      );
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute(
+        'download',
+        `${mod.name}.zip`,
+      );
+
+      // Append to html link element page
+      document.body.appendChild(link);
+
+      // Start download
+      link.click();
+
+      // Clean up and remove the link
+      link.parentNode.removeChild(link);
+    }
+
+    download();
+    
+  }
+
   if(isLoading){
     return(
       <Fragment>
@@ -37,6 +67,7 @@ const Mod = () => {
       <p>Description:</p>
       <p>{mod.description}</p>
       {mod.additionalInfo && <p>Additional info: {mod.additionalInfo}</p>}
+      <button onClick={downloadFile}>Download</button>
     </Fragment>
   )
 }
