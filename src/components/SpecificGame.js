7b79b57b1b123b1eb4092376703af51958f2e642
sly-agustin <agustin_sly@hotmail.com>
2023-04-20 15:27:30 -0300

    feat: added search mod bar

--- src/components/SpecificGame.js
+++ src/components/SpecificGame.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState, useContext } from "react";
+import { useForm } from "react-hook-form";
 import { Link, useParams } from 'react-router-dom'
 import noimage from '../assets/no-image-icon.png'
 import loadingGif from '../assets/loading.gif'
@@ -9,9 +10,48 @@ const SpecificGame = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [mods, setMods] = useState([]);
   const [gameName, setGameName] = useState();
+  const [data, setData] = useState();
+  const [searched, setSearched] = useState(false);
+  const [messages, setMessages] = useState();
   const {isLoggedOn} = useContext(AuthContext);
 
   const { id } = useParams();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleInputDataChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const onSubmit = (data) => {
+    const search = async () => {
+      try{
+        const response = await fetch(`${process.env.REACT_APP_HOST}/mod/game/${id}/search`,{
+          method: "POST",
+          headers: {
+            Accept: 'application/json',
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        const responseJson = await response.json();
+        if(response.ok){
+          setMods(responseJson);
+          setSearched(true);
+        }
+        else{
+          setMessages('request not ok '+responseJson)
+        }
+      }
+      catch(err){
+        setMessages('Errors ocurred: '+err)
+      }
+    }
+   
+    search();
+  }
 
   useEffect(() => {
     const getMods = async() => {
@@ -52,8 +92,23 @@ const SpecificGame = () => {
     <Fragment>
       <div className="container">
       <h2 className="mb-4">Viewing mods for {gameName}</h2>
+      <form className="form-inline my-2 my-lg-0" onSubmit={handleSubmit(onSubmit)}>
+        <input className="form-control mr-sm-2" 
+          type="search" 
+          placeholder="Search" 
+          aria-label="Search"
+          id="modName" 
+          {...register("name", {
+            required: 'Mod name required', 
+            minLength: {value: 3, message: 'Min length for search is 3 characters'}, 
+            maxLength: {value: 100, message: 'Max length for search is 100 characters'}})} 
+            onChange={handleInputDataChange}
+        ></input>
+        <p className="text-danger">{errors.name?.message}</p>
+        <button className="btn button-dark my-2 my-sm-0">Search by name</button>
+      </form>
       {isLoggedOn && <p>Want to upload your mod? <Link className="btn button-dark ms-4" to={`upload`}>Let's do it!</Link></p>}
-      <div className="container d-flex">
+      <div className="container d-flex mt-3">
         <div className="row">
         {
           mods.map((mod) => 
